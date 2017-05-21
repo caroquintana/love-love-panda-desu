670fbd980383cc83291ffcfb4c5065f0389d4b4b
caroquintana <caro.quintana.n@gmail.com>
2017-05-21 02:25:24 -0400

    fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var imgs = document.getElementById("arreglopandas");
+
+pictures.forEach(function (e) {
+	var newTarget= document.createElement("li");
+	var newImage = document.createElement("img");
+	newImage.setAttribute("class", "image");
+	newImage.setAttribute("src", e);
+	newTarget.appendChild(newImage);
+	imgs.appendChild(newTarget);
+});
